e00654e012bc0a86d0cf994cbf4a5571ef5e9887
Ayzat Murtazin <ayzat.murtazin@setinbox.com>
2024-05-12 18:34:59 +0200

    refactor: fix nullable fields in EventUpdateRequest

--- ewm-service/src/main/java/ru/practicum/common/dto/EventUpdateRequest.java
+++ ewm-service/src/main/java/ru/practicum/common/dto/EventUpdateRequest.java
@@ -7,7 +7,6 @@ import lombok.experimental.FieldDefaults;
 import ru.practicum.common.enums.StateAction;
 import ru.practicum.common.validation.ValidStartDate;
 
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 import javax.validation.constraints.PositiveOrZero;
 import javax.validation.constraints.Size;
